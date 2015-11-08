2d0378c06401390f5608ac7f54a0c07ca62bbedf
OriRoth <ori.rothh@gmail.com>
2015-11-08 20:43:05 +0200

    Update system.h

--- code/system.h
+++ code/system.h
@@ -1,56 +1,33 @@
 #ifndef SYSTEM_H_
 #define SYSTEM_H_
 
-#include "memmanage.h"
-#include <string.h>
+#include "memory.h"
 
-#define ERROR_MESSAGE_BUFFER_SIZE 64
-#define HASH_FACTOR 8
-#define BUILTIN_BUFFER_SIZE 100 // TODO: give exact size
-#define SECURITY_INIT()	security_head = security_wait = nil
-#define SECURE(address)	do {												\
-							security_wait = address;						\
-							memptr temp;									\
-							if (security_head == nil) {						\
-								security_head = new_data();					\
-								MemoryPool[security_head].carKind = CONS;	\
-								MemoryPool[security_head].car = address;	\
-								MemoryPool[security_head].cdrKind = NIL;	\
-							} else {										\
-								temp = security_head;						\
-								security_head = new_data();					\
-								MemoryPool[security_head].carKind = CONS;	\
-								MemoryPool[security_head].car = address;	\
-								MemoryPool[security_head].cdrKind = CONS;	\
-								MemoryPool[security_head].cdr = temp;		\
-							}												\
-							security_wait = nil;							\
-						} while(0)
-#define ERROR(msg)	do {													\
-						error = true;										\
-						strcpy(&error_message, msg);						\
-						error_message[ERROR_MESSAGE_BUFFER_SIZE - 1] = 0;	\
-						} while(0)
+#define NATIVE_COUNT 16
+#define NATIVE_NAME_LIMIT 8
+memptr native_identifiers[NATIVE_COUNT];
+char native_names_buffer[NATIVE_NAME_LIMIT * NATIVE_COUNT];
+
+typedef enum {
+	ENVIRONMENT, DATA
+} LookUp;
 
 memptr environment;
 memptr nil;
 memptr t;
-typedef memptr (*builtin)(memptr func_expr, memptr first_arg);
-bool error;
-char error_message[ERROR_MESSAGE_BUFFER_SIZE];
 memptr security_head;
-memptr security_wait;
+memptr security_tail;
+memptr lambda_identifier;
+memptr symbol_identifier;
+memptr handler_identifier;
 
-// builtin functions container
-builtin builtins[BUILTIN_BUFFER_SIZE];
+// builtin functions addresses container
+memptr builtins[NATIVE_COUNT];
 
 // initialize the system
 void system_initialize();
 
-// prints the first m memory cells of the cons pool
-void print_mem(int m);
-
-memptr translate_func_expr(memptr func_expr);
-memptr resolve_expr(memptr expr);
+// resolve an expression, returns evaluation result
+memptr resolve_expression(memptr expr);
 
 #endif /* SYSTEM_H_ */
